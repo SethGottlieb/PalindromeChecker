9741ad551f8b6504ba780befcad4109339dc9605
unknown <segottlieb@697-334-Dale.sf.k12.internal>
2015-01-09 10:22:52 -0800

    Created program

--- build-tmp/source/PalindromeChecker.java
+++ build-tmp/source/PalindromeChecker.java
@@ -32,7 +32,24 @@ public void setup()
 }
 public boolean palindrome(String word)
 {
-  //your code here
+  String noCaps = new String(word.toLowerCase());
+  String noSpace = new String();
+  String reversed = new String();
+  for (int i = 0; i < noCaps.length(); i++) 
+  {
+    if(Character.isLetter(noCaps.charAt(i)) == true) 
+    {
+      noSpace += noCaps.charAt(i);
+    }
+  }
+  for (int i = noSpace.length()-1; i >= 0 ; i--) 
+  {
+    reversed += noSpace.charAt(i);
+  }
+  if(noSpace.equals(reversed))
+  {
+    return true;
+  }
   return false;
 }
 
